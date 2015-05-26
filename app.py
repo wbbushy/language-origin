f82ee13a7f4ca041763d8290a55030403d6f57ea
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-05-26 14:04:15 -0700

    added import closing from contextlib Added init_db() function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/language-origin.db'
@@ -22,5 +23,16 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
